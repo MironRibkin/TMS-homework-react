dff7fecf31ad3d39474c930845d914ab787c631d
mironrybkin <miron.ribkin98@mail.ru>
2022-04-19 17:54:44 +0300

    Text react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {Text} from "./text";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Text text="Hello World" font={500} color='#545231' underline={true}/>
+        </div>
+    );
 }
 
 export default App;
