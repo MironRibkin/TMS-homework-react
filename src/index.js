304d80eed413e64f7ff78dcc6b9beed669c6a8cd
mironrybkin <miron.ribkin98@mail.ru>
2022-04-24 11:04:11 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import {TaskList} from "./taskList";
 
 ReactDOM.render(
     <React.StrictMode>
-        <TaskList done={true} number={1 + '.'} text='Написать список'/>
-        <TaskList done={false} number={2 + '.'} text='Зачеркнуть первый вариант'/>
-        <TaskList done={false} number={3 + '.'} text='Сделать вид, что очень занят работой'/>
-        <TaskList done={false} number={4 + '.'} text='Придумать, что-нибудь в четвёртом пункте'/>
-        <TaskList done={false} number={5 + '.'} text='Сделать вид, что придумал оригинальный четвертый пункт'/>
+        <TaskList done={true} number={1} text='Написать список'/>
+        <TaskList done={false} number={2} text='Зачеркнуть первый вариант'/>
+        <TaskList done={false} number={3} text='Сделать вид, что очень занят работой'/>
+        <TaskList done={false} number={4} text='Придумать, что-нибудь в четвёртом пункте'/>
+        <TaskList done={false} number={5} text='Сделать вид, что придумал оригинальный четвертый пункт'/>
         <App/>
     </React.StrictMode>,
     document.getElementById('root')
