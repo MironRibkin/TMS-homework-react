5ba62686b30bd305c134d9dc6ee4abf57763d08e
mironrybkin <miron.ribkin98@mail.ru>
2022-05-01 15:12:47 +0300

    Cursive-text

--- src/Cursive-text.js
+++ src/Cursive-text.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 
 export const ItalicText = (props) => {
     const [done, setDone] = useState(props.done);
+    const [toggle, setToggle] = useState(false)
+    const [pos, setPos] = useState({x: 0, y: 0});
     const toggleCheckbox = () => {
         setDone(prev => !prev)
     }
@@ -9,16 +11,50 @@ export const ItalicText = (props) => {
     const style = {
         display: 'flex',
         alignItems: 'center',
+        flexDirection: 'column',
         width: '90%',
         height: '90%',
-        fontsize: '60px',
+        fontSize: '30px',
         fontWeight: '600',
-        cursor: 'pointer',
         margin: '20px',
     }
 
+    const ButtonStyle = {
+        width: "60px",
+        height: "60px",
+        fontSize: '40px',
+        fontWeight: '600',
+        cursor: 'pointer',
+        position:'relative'
+    }
+
+    const divStyle = {
+        position: 'absolute',
+        padding: '10px',
+        backgroundColor:'#ff00ff',
+        width: 'fit-content',
+        height: 'fit-content',
+        fontSize:'15px',
+        display: toggle ? 'block' : 'none',
+        top: `${pos.y + 5}px`,
+        left: `${pos.x + 2}px`
+    }
+
+    const onMouseOver = e => {
+        setToggle(true);
+        setPos({x:e.clientX, y: e.clientY})
+    }
+
+    const onMouseLeave = () => {
+        setToggle(false)
+    }
+
     return (
-        <div style={style} onClick={toggleCheckbox}>
+        <div style={style}>
+            <button style={ButtonStyle} onClick={toggleCheckbox} onMouseOver={(e) => onMouseOver(e)} onMouseLeave={onMouseLeave}>I</button>
+            <div style={divStyle}>
+                Cursive
+            </div>
             <h1 className='text' style={{fontFamily: done ? 'none' : 'Noto Sans Old Italic, sans-serif'}}>
                 {props.text}
             </h1>
