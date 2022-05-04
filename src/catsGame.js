3ce1509d03ba7cebf2acc68f611d26837c4dc74c
mironrybkin <miron.ribkin98@mail.ru>
2022-05-04 19:06:54 +0300

    Cursive-text

---
+++ src/catsGame.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const CatsGames = () => {
+    const style = {
+        border: '1px solid red',
+        radius: "50%",
+        width: '25px',
+        height: '25px',
+        backgroundColor: 'black',
+
+    }
+    return (
+        <div style={style}>
+            123
+        </div>
+    )
+}
