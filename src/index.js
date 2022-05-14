9c27368641254eae43dcd0c41ef886d0434fc873
mironrybkin <miron.ribkin98@mail.ru>
2022-05-14 22:11:48 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {CatsGames} from "./catsGame";
 
 ReactDOM.render(
   <React.StrictMode>
-      <CatsGames>
-          <div className='catsDiv'/>
-      </CatsGames>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
