e6cf157b1ad72b029cf7ac9db879012d6cba6b45
mironrybkin <miron.ribkin98@mail.ru>
2022-05-14 20:33:01 +0300

    cats game

--- src/catsGame.js
+++ src/catsGame.js
@@ -2,27 +2,48 @@ import React, {useState} from "react";
 
 export const CatsGames = () => {
 
-    const style = {
+    const defaultBtnStyle = {
         border: '1px solid red',
         borderRadius: "50%",
-        width: '250px',
-        height: '250px',
+        width: '150px',
+        height: '150px',
         backgroundColor: 'red',
         display: 'flex',
-        justifyContent:'center',
+        justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 34
+        fontSize: 34,
+        position: 'absolute',
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        top: 0,
+        left: 0,
+
     }
 
-    const [use, setUse] = useState(style);
+    const wrapperStyle = {
+        width: '1000px',
+        height: '1000px',
+        border: '1px solid black',
+        position: 'relative',
+    }
 
-    const cats = () =>{
-        setUse({...use, backgroundColor: 'yellow'})
+
+    const [btnStyle, setBtnStyle] = useState(defaultBtnStyle);
+
+    const random = {
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+            top: Math.floor(Math.random() * 850),
+            left: Math.floor(Math.random() * 850)
+        }
+
+
+    const handleClick = () => {
+        setBtnStyle({...btnStyle, backgroundColor: random.color, top: random.top, left: random.left})
     }
 
     return (
-        <div>
-            <div style={use} onClick={cats}>Meow
+        <div style={wrapperStyle}>
+            <div style={btnStyle} onClick={handleClick}>Meow
             </div>
         </div>
     )
